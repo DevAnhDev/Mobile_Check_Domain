7cb7945fe698e7aedb25a449f33ba005a4ca0393
Ho Van Trung <trungho@gmail.com>
2024-08-23 20:46:57 -0700

    config(iporoject):

--- src/constain/index.js
+++ src/constain/index.js
@@ -1,4 +1,6 @@
 export const apiURL = "http://51.79.208.204"
+// export const apiURL = "http://192.168.1.10:8080"
+
 export const DOMAIN = "/api/domains"
 export const MOBI = "/api/mobile"
 export const VIETEL = "/api/viettel"
